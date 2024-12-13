4b09be5f94531a3ab089560c88b57ec6c7f9c929
skynightgamer <125209539+skynightgamer@users.noreply.github.com>
2024-12-13 12:02:50 +0000

    add main.html

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res)=>{
-    res.status(200);
-    res.send("Welcome to root URL of Server");
+    res.sendFile(__dirname + '/public/main.html');
 });
 
 app.listen(PORT, (error) =>{
     if(!error)
-        console.log("Server is Successfully Running, "+ PORT)
+        console.log("Server is successfully running on "+ PORT)
     else 
         console.log("Error occurred, server can't start", error);
     }
