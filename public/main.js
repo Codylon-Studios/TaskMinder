d720ba4909dfb771f72e484d2058784147738d89
Fabian Leonardi <fabian@bigigloo.de>
2024-12-29 17:26:20 +0100

    Added basic layout

--- public/main.js
+++ public/main.js
@@ -13,7 +13,26 @@ function deleteNote(/*parameters*/) {
   socket.emit('delete-note', /*parameters*/);
 }
 
+function updateTimetableMode() {
+  if ($("#timetable-mode-less")[0].checked) {
+    $("#timetable-less")[0].classList.remove("d-none");
+    $("#timetable-more")[0].classList.add("d-none");
+  }
+  else {
+    $("#timetable-less")[0].classList.add("d-none");
+    $("#timetable-more")[0].classList.remove("d-none");
+  }
+}
+
 // Listen for the current list from the server
 socket.on('current-notes', (/*parameters*/) => {
   //render the notes list
+});
+
+updateTimetableMode();
+
+$("#timetable-mode input").each(() => {
+  $(this).on("click", () => {
+    updateTimetableMode();
+  });
 });
