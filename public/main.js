eb6135611438a75bd6933b008dfb152e9c1c9c7e
Fabian Leonardi <fabian@bigigloo.de>
2024-12-30 11:26:17 +0100

    Added responsive hamburger menu in navbar, added selecton for homework, added coloful event boxes, added light / dark mode in settings

--- public/main.js
+++ public/main.js
@@ -24,15 +24,31 @@ function updateTimetableMode() {
   }
 }
 
+function updateColorTheme() {
+  if ($("#color-theme-dark")[0].checked) {
+    document.body.setAttribute("data-bs-theme", "dark");
+  }
+  else {
+    document.body.setAttribute("data-bs-theme", "light");
+  }
+}
+
 // Listen for the current list from the server
 socket.on('current-notes', (/*parameters*/) => {
   //render the notes list
 });
 
 updateTimetableMode();
+updateColorTheme();
 
 $("#timetable-mode input").each(() => {
   $(this).on("click", () => {
     updateTimetableMode();
+  });
+});
+
+$("#color-theme input").each(() => {
+  $(this).on("click", () => {
+    updateColorTheme();
   });
 });
