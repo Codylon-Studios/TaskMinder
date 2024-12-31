64ede13d7de0eba5ce0e2272826c400c788721d6
Fabian Leonardi <fabian@bigigloo.de>
2024-12-31 17:00:25 +0100

    Added user feedback to login & register

--- public/main.js
+++ public/main.js
@@ -48,6 +48,23 @@ function resetLoginRegisterModal() {
   $("#login-register-back-button").addClass("d-none");
   $("#login-button").addClass("d-none");
   $("#register-button").addClass("d-none");
+
+  $("#login-register-error-invalid-username").addClass("d-none");
+  $("#login-register-error-invalid-username").removeClass("d-flex");
+
+  $("#login-error-invalid-password").addClass("d-none");
+  $("#login-error-invalid-password").removeClass("d-flex");
+
+  $("#register-error-insecure-password").addClass("d-none");
+  $("#register-error-insecure-password").removeClass("d-flex");
+}
+
+function checkUsername(username) {
+  return /^\w{4,20}$/.test(username);
+}
+
+function checkSecurePassword(password) {
+  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+={}[\]:;"'<>,.?/-]).{8,}$/.test(password);
 }
 
 const socket = io();
@@ -84,12 +101,58 @@ $("#login-register-modal").on("show.bs.modal", () => {
   $("#login-register-next-button").addClass("disabled");
 });
 
+
+// Check username
+
 $("#login-register-username").on("input", () => {
-  if ($("#login-register-username").val() != "") {
+  if (checkUsername($("#login-register-username").val())) {
     $("#login-register-next-button").removeClass("disabled");
+    $("#login-register-error-invalid-username").addClass("d-none");
+    $("#login-register-error-invalid-username").removeClass("d-flex");
   }
-  else {
+});
+
+$("#login-register-username").on("change", () => {
+  if (! checkUsername($("#login-register-username").val())) {
     $("#login-register-next-button").addClass("disabled");
+    $("#login-register-error-invalid-username").removeClass("d-none");
+    $("#login-register-error-invalid-username").addClass("d-flex");
+  }
+});
+
+
+// Check password
+
+$("#register-password").on("input", () => {
+  if (checkSecurePassword($("#register-password").val())) {
+    $("#register-error-insecure-password").addClass("d-none");
+    $("#register-error-insecure-password").removeClass("d-flex");
+  }
+});
+
+$("#register-password").on("change", () => {
+  if (! checkSecurePassword($("#register-password").val())) {
+    $("#register-error-insecure-password").removeClass("d-none");
+    $("#register-error-insecure-password").addClass("d-flex");
+  }
+});
+
+
+// Check repeated password
+
+$("#register-password-repeat").on("input", () => {
+  if ($("#register-password").val() == $("#register-password-repeat").val()) {
+    $("#register-button").removeClass("disabled");
+    $("#register-error-no-matching-passwords").addClass("d-none");
+    $("#register-error-no-matching-passwords").removeClass("d-flex");
+  }
+});
+
+$("#register-password-repeat").on("change", () => {
+  if ($("#register-password").val() != $("#register-password-repeat").val()) {
+    $("#register-button").addClass("disabled");
+    $("#register-error-no-matching-passwords").removeClass("d-none");
+    $("#register-error-no-matching-passwords").addClass("d-flex");
   }
 });
 
@@ -119,11 +182,58 @@ $("#login-button").on("click", () => {
   let username = $("#login-register-username").val();
   let password = $("#login-password").val()
   console.log("Login", username, password)
+
+  let url = "/account/login";
+  let data = {
+      username: username,
+      password: password
+  };
+  let hasResponded = false;
+  $.post(url, data, function (result, status) {
+    hasResponded = true;
+    if (result == "0") {
+      $("#login-success-toast .username").html(username);
+      $("#login-success-toast").toast("show");
+    }
+    else if (result == "1") {
+      $("#error-server-toast").toast("show");
+    }
+    else if (result == "2") {
+      $("#login-error-invalid-password").removeClass("d-none");
+      $("#login-error-invalid-password").addClass("d-flex");
+    }
+  });
+  setTimeout(() => {
+    if (!hasResponded) {
+      $("#error-server-toast").toast("show");
+    }
+  }, 1000);
 });
 
 $("#register-button").on("click", () => {
   let username = $("#login-register-username").val();
   let password = $("#register-password").val()
-  let passwordRepeat = $("#register-password-repeat").val()
-  console.log("Register", username, password, passwordRepeat)
+  console.log("Register", username, password)
+
+  let url = "/account/register";
+  let data = {
+      username: username,
+      password: password
+  };
+  let hasResponded = false;
+  $.post(url, data, function (result, status) {
+    hasResponded = true;
+    if (result == "0") {
+      $("#register-success-toast .username").html(username);
+      $("#register-success-toast").toast("show");
+    }
+    else if (result == "1") {
+      $("#error-server-toast").toast("show");
+    }
+  });
+  setTimeout(() => {
+    if (!hasResponded) {
+      $("#error-server-toast").toast("show");
+    }
+  }, 1000);
 });
