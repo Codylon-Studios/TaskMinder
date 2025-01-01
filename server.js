1477b1833985fe87245e44b5e516e5bd5653bb29
Mingqi Li <mingqi_li@icloud.com>
2025-01-01 20:49:57 +0100

    globalize socket instance on client side; split socket part in backend for modularity

--- server.js
+++ server.js
@@ -1,31 +1,24 @@
-// Import necessary modules: express, http server, socket.io
 const express = require('express');
 const { createServer } = require('http');
 const auth = require('./routes/auth');
 const session = require('express-session');
+const initializeSocket = require('./socket');
 
 const app = express();
 const server = createServer(app);
-const io = require('socket.io')(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    transports: ['websocket', 'polling'],
-    allowedHeaders: ['Access-Control-Allow-Origin'],
-    credentials: true
-  },
-});
+
+// Initialize Socket.IO
+initializeSocket(server);
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log('Server running at http://localhost:3000');
 });
-
 
 // Middleware to parse request bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-//configure session
+// Configure session
 app.use(session({
   secret: "notsecret",
   resave: false,
@@ -34,49 +27,13 @@ app.use(session({
   name: 'UserLogin',
 }));
 
-
 app.use('/account', auth);
-
-
-// When a user connects
-io.on('connection', (socket) => {
-  console.log('A user connected');
-  //Send current notes list
-  socket.emit('current-notes', /*data*/);
-
-
-  // ADD ITEM
-  socket.on('add-note', (/*data*/) => {
-    // Add new note to database
-    // Broadcast new note to all clients
-    io.emit('current-notes', /*data*/);
-  });
-
-  // EDIT ITEM
-  socket.on('edit-note', (/*data*/) => {
-    // Update note in database
-    // Broadcast updated note-list to all clients
-    io.emit('current-notes', /*data*/);
-  });
-
-  // DELETE ITEM
-  socket.on('delete-note', (itemValue) => {
-    //remove note from database
-    // Broadcast updated note-list to all clients
-    io.emit('current-notes', /*data*/);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
 
 // Serve index.html when root URL is accessed
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/main/main.html');
 });
-
-// Serve index.html when root URL is accessed
+// Serve allhomework.html when URL ../allhomework is accessed
 app.get('/allhomework', function (req, res) {
   res.sendFile(__dirname + '/public/allhomework/allhomework.html');
 });
