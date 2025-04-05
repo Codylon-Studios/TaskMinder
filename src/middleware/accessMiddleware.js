60ab60d0e2a550ec63db594a9057960e65016211
Mingqi Li <mingqi_li@icloud.com>
2025-04-05 23:23:37 +0200

    rm navbar from join, add compression and preload

--- src/middleware/accessMiddleware.js
+++ src/middleware/accessMiddleware.js
@@ -1,9 +1,9 @@
 // Middleware to enforce session-based access control
 const checkAccess = (req, res, next) => {
-    if (req.session.account || req.session.classcode === process.env.CLASSCODE) {
+    if (req.session.account || (req.session.classcode && req.session.classcode === process.env.CLASSCODE)) {
       return next();
     }
-    return res.redirect(308, '/join');
+    return res.redirect(302, '/join');
   }
 
 
