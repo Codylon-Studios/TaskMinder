95ec62ec8106f91c9edddc8cbda342fb4e87c29d
Fabian Leonardi <fabian@bigigloo.de>
2025-06-16 11:26:33 +0200

    Moved bottombar up

--- frontend/src/snippets/bottombar/bottombar.ts
+++ frontend/src/snippets/bottombar/bottombar.ts
@@ -1,14 +1,31 @@
+import { user } from "../navbar/navbar.js"
+
+if (/OS (18|26)(_\d+)* like Mac OS X/.test(navigator.userAgent)) {
+  $(".bottombar").css("padding-bottom", "1rem")
+}
+
 const availableLinks = [
   "/about",
   "/homework",
   "/main",
   "/events",
-  "/settings"
+  "/settings",
+  "/join"
 ]
-const siteIndex = availableLinks.indexOf(location.pathname.replace(/\/$/, "")) ?? -1
+const siteName = location.pathname.replace(/\/$/, "")
+const siteIndex = availableLinks.indexOf(siteName) ?? -1
+
+if (! user.classJoined) {
+  $(".bottombar-link").eq(1).addClass("bottombar-link-deactivated")
+  $(".bottombar-link").eq(3).addClass("bottombar-link-deactivated")
+}
 
 $(".bottombar-link").eq(siteIndex).addClass("bottombar-current-link")
+if (siteIndex == 5) {
+  $(".bottombar-link").eq(2).addClass("bottombar-current-link")
+}
 $(".bottombar-link").each(function (id) {
+  if ($(this).is(".bottombar-link-deactivated")) return
   $(this).on("click", () => {
     location.href = availableLinks[id]
   })
--- frontend/src/snippets/footer/footer.ts
+++ frontend/src/snippets/footer/footer.ts
@@ -1,16 +1,22 @@
 import { colorTheme } from "../../global/global.js"
 
+const bottombarShown = window.innerWidth < 992 && $(".bottombar").length > 0
+let bottombarHeight = 45
+if (/OS (18|26)(_\d+)* like Mac OS X/.test(navigator.userAgent)) {
+  bottombarHeight += 16
+}
+
 if (localStorage.getItem("displayFooter") == "false") {
   $("footer").hide()
-  $("body").css({paddingBottom: ((window.innerWidth >= 992) ? 0 : 70) + 70 + "px"})
+  $("body").css({paddingBottom: (bottombarShown ? bottombarHeight : 0) + 70 + "px"})
 }
 else {
-  $("body").css({paddingBottom: ((window.innerWidth >= 992) ? 0 : 70) + "px"})
+  $("body").css({paddingBottom: (bottombarShown ? bottombarHeight : 0) + "px"})
 }
 
 $("#footer-close").on("click", () => {
   localStorage.setItem("displayFooter", "false")
-  $("body").css({paddingBottom: ((window.innerWidth >= 992) ? 0 : 70) + 70 + "px"})
+  $("body").css({paddingBottom: (bottombarShown ? bottombarHeight : 0) + 70 + "px"})
   $("footer").hide()
   if (["/settings", "/settings/"].includes(location.pathname)) {
     $("#display-footer input").prop("checked", false)
